bb9d805edeb1f1e71bd5369b099fa0fb9fbf51df
MeshachArinze <meshacharinze@gmail.com>
2023-07-29 11:47:27 +0100

    commit to repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App(props) {
+  const currDate = new Date();
+
+  return (
+    <div>
+      <h1>Meshach Ekene</h1>
+      <h2>
+        It is {currDate.toLocaleDateString()} and the time now is{" "}
+        {currDate.toLocaleTimeString()}.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
